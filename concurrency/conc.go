09112e6eb0d2be5724972c4b96a77901e581f50b
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2022-12-10 18:37:28 +0600

    Add exercise files on Concurrency

---
+++ concurrency/conc.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(2)
+
+	go func() {
+		defer waitGroup.Done()
+		time.Sleep(5 * time.Second)
+		fmt.Println("Hello")
+	}()
+
+	go func() {
+		defer waitGroup.Done()
+		fmt.Println("Pluralsight")
+	}()
+
+	waitGroup.Wait()
+}
